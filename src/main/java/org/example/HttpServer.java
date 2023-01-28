f03b656b155c2c04654554510f6235a7ea9711af
Andrés Ariza <andres-ariza70@hotmail.com>
2023-01-28 14:14:13 -0500

    request

--- src/main/java/org/example/HttpServer.java
+++ src/main/java/org/example/HttpServer.java
@@ -31,18 +31,29 @@ public class HttpServer {
                             clientSocket.getInputStream()));
             String inputLine, outputLine;
 
-
-
+            boolean first_line = true;
+            String request = "/simple";
             while ((inputLine = in.readLine()) != null) {
+                if (first_line) {
+                    request = inputLine.split(" ")[1];
+                    first_line = false;
+                }
                 System.out.println("Received: " + inputLine);
                 if (!in.ready()) {
                     break;
                 }
             }
-            outputLine = "HTTP/1.1 200 OK\r\n"
+            if (request.startsWith("/json")) {
+                outputLine = jsonSimple();
+            } else {
+                outputLine = htmlGetForm();
+            }
+
+            /*outputLine =
+                    // "HTTP/1.1 200 OK\r\n"
                     //+ "Content-type: text/html\r\n"
                     //+ "\r\n"
-                    + htmlGetForm();
+                    jsonSimple();*/
             out.println(outputLine);
             out.close();
             in.close();
@@ -52,7 +63,8 @@ public class HttpServer {
     }
 
     public static String htmlSimple() {
-        return "Content-type: text/html\r\n" +
+        return "HTTP/1.1 200 OK\r\n" +
+                "Content-type: text/html\r\n" +
                 "\r\n" +
                 "<!DOCTYPE html>"
                 + "<html>"
@@ -67,13 +79,15 @@ public class HttpServer {
     }
 
     public static String jsonSimple() {
-        return "Content-type: application/json\r\n"+
+        return "HTTP/1.1 200 OK\r\n" +
+                "Content-type: application/json\r\n"+
                 "\r\n" +
                 "{\"Title\":\"Guardians of the Galaxy Vol. 2\",\"Year\":\"2017\",\"Rated\":\"PG-13\",\"Released\":\"05 May 2017\",\"Runtime\":\"136 min\",\"Genre\":\"Action, Adventure, Comedy\",\"Director\":\"James Gunn\",\"Writer\":\"James Gunn, Dan Abnett, Andy Lanning\",\"Actors\":\"Chris Pratt, Zoe Saldana, Dave Bautista\",\"Plot\":\"The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father the ambitious celestial being Ego.\",\"Language\":\"English\",\"Country\":\"United States\",\"Awards\":\"Nominated for 1 Oscar. 15 wins & 60 nominations total\",\"Poster\":\"https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg\",\"Ratings\":[{\"Source\":\"Internet Movie Database\",\"Value\":\"7.6/10\"},{\"Source\":\"Rotten Tomatoes\",\"Value\":\"85%\"},{\"Source\":\"Metacritic\",\"Value\":\"67/100\"}],\"Metascore\":\"67\",\"imdbRating\":\"7.6\",\"imdbVotes\":\"687,044\",\"imdbID\":\"tt3896198\",\"Type\":\"movie\",\"DVD\":\"22 Aug 2017\",\"BoxOffice\":\"$389,813,101\",\"Production\":\"N/A\",\"Website\":\"N/A\",\"Response\":\"True\"}";
     }
 
     public static String htmlGetForm() {
-        return "Content-type: text/html\r\n" +
+        return "HTTP/1.1 200 OK\r\n" +
+                "Content-type: text/html\r\n" +
                 "\r\n" +
                 "<!DOCTYPE html>\n" +
                 "<html>\n" +
